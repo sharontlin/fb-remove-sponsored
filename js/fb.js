633d4db5c9851300bf8790f6ac746ca2e0f465fc
Sharynne T Lin <sharonlin14@gmail.com>
2020-01-30 22:03:29 -0800

    Added files for Chrome extension

---
+++ js/fb.js
@@ -0,0 +1,43 @@
+function clean_the_shit(){
+	// updated values from the chrome storage
+	chrome.storage.local.get(["remove_sponsored_posts", "remove_pop_across_fb_posts", "sponsored_count", "pop_count"], function(item) {
+	  	remove_sponsored_posts = item.remove_sponsored_posts;
+		remove_pop_across_fb_posts = item.remove_pop_across_fb_posts;
+		sponsored_count = item.sponsored_count;
+		pop_count = item.pop_count;
+		// console.log('start:', remove_sponsored_posts, remove_pop_across_fb_posts, sponsored_count, pop_count);
+		if (remove_sponsored_posts || remove_pop_across_fb_posts){
+			// get all the posts
+			elems = document.querySelectorAll("[id^='hyperfeed_story_id_']");
+			// loop over all the posts
+			// console.log(elems.length);
+			for (let x = 0; x < elems.length; x++){
+				if(remove_sponsored_posts){ // remove sponsored
+					//if (elems[x].innerText.indexOf("SpSonSsoSredS") >= 0){
+					if (elems[x].innerHTML.indexOf("lock img sp_RLFL6-1bUHS_2x sx_60885d") >= 0){
+						elems[x].remove();
+						sponsored_count += 1;
+					}
+				}
+				if(remove_pop_across_fb_posts){ //  remove popular across facebook posts
+					if (elems[x].innerHTML.indexOf("lock img sp_RLFL6-1bUHS_2x sx_af9dce") >= 0){
+						elems[x].remove();
+						pop_count += 1;
+					}
+				}
+			}
+		}
+		// console.log('count:', sponsored_count, pop_count);
+	    chrome.storage.local.set({
+	      "sponsored_count": sponsored_count,
+	      "pop_count": pop_count
+	    });
+	});
+}
+
+setInterval(
+	() => clean_the_shit(),
+	500
+);
+
+// about:debugging
